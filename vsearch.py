eb43306a03f84509b105d00200ac625f593d0044
samoylovivan <52442440+samoylovivan@users.noreply.github.com>
2023-12-16 06:49:36 +0200

    vsearch.py

--- vsearch.py
+++ vsearch.py
@@ -2,6 +2,4 @@
 def search4vowels(word):
     """Display any vowels found in asked-for word"""
     vowels = set('aeiou')
-    found = vowels.intersection(set(word))
-    for vowel in found:
-        print(vowel)
+    return vowels.intersection(set(word))
