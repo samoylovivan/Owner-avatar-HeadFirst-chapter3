b760a7cb5e284cbab21a514a82447e0563acfda2
samoylovivan <52442440+samoylovivan@users.noreply.github.com>
2023-12-16 17:37:29 +0200

    vsearch.py

--- vsearch.py
+++ vsearch.py
@@ -1,5 +1,5 @@
 
-def search4vowels(word):
+def search4vowels(word: str) -> set:
     """Display any vowels found in asked-for word"""
     vowels = set('aeiou')
     return vowels.intersection(set(word))
