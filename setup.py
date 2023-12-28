bc90a2437705dc6479ec5055be2a64a3776ce4a9
samoylovivan <52442440+samoylovivan@users.noreply.github.com>
2023-12-28 19:58:01 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup (
+    name='vsearch',
+    version='1.0',
+    description='The Head First Python Search Tools',
+    author='HF Python 2e',
+    author_email='hppy2e@gmail.com',
+    url='headfirstlabs.com',
+    py_modules=['vsearch'],
+    )
