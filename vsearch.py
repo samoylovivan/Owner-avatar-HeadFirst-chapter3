067902241f86889543aedcbbfafd100c354cd8b0
samoylovivan <52442440+samoylovivan@users.noreply.github.com>
2024-01-06 21:07:37 +0200

    vsearch.py

--- vsearch.py
+++ vsearch.py
@@ -4,6 +4,6 @@ def search4vowels(word: str) -> set:
     vowels = set('aeiou')
     return vowels.intersection(set(word))
 
-def search4letters(phrase:str, letters:str='aeiou') -> set:
+def search4letters(phrase: str, letters: str='aeiou') -> set:
     """Return a set of the 'letters' found in 'phrase'"""
     return set(letters).intersection(set(phrase))
